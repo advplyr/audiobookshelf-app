c5a9677ac613ec20bfee7f4e8818ef27eb139dbc
advplyr <advplyr@protonmail.com>
2022-04-23 14:38:29 -0500

    Fix cover aspect ratio #136

--- pages/bookshelf/index.vue
+++ pages/bookshelf/index.vue
@@ -20,7 +20,6 @@
             <span class="material-icons text-error text-lg">cloud_off</span>
             <p class="pl-2 text-error text-sm">Audiobookshelf server not connected.</p>
           </div>
-          <!-- <p class="px-4 text-center text-error absolute bottom-12 left-0 right-0 mx-auto"><strong>Important!</strong> This app requires that you are running <u>your own server</u> and does not provide any content.</p> -->
         </div>
         <div class="flex justify-center">
           <ui-btn v-if="!user" small @click="$router.push('/connect')" class="w-32">Connect</ui-btn>
@@ -28,7 +27,7 @@
       </div>
     </div>
     <div v-if="loading" class="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-      <ui-loading-indicator text="Loading Library...">
+      <ui-loading-indicator text="Loading Library..." />
     </div>
   </div>
 </template>
