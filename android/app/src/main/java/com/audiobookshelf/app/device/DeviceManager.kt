107c0b49c10fadfc17b38f619bffdc1c81dca6c3
advplyr <advplyr@protonmail.com>
2022-05-24 09:08:23 -0500

    Fix:Base64 encoding include URL_SAFE flag to prevent having / in local library item ids #192

--- android/app/src/main/java/com/audiobookshelf/app/device/DeviceManager.kt
+++ android/app/src/main/java/com/audiobookshelf/app/device/DeviceManager.kt
@@ -24,6 +24,6 @@ object DeviceManager {
   }
 
   fun getBase64Id(id:String):String {
-    return android.util.Base64.encodeToString(id.toByteArray(), android.util.Base64.NO_WRAP)
+    return android.util.Base64.encodeToString(id.toByteArray(), android.util.Base64.URL_SAFE or android.util.Base64.NO_WRAP)
   }
 }
