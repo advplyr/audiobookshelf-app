f0f2a5361096eb2215f7d39ee00553bb40607f37
advplyr <advplyr@protonmail.com>
2022-06-02 17:57:37 -0500

    Update:Item page redesign with more details like narrator and chapters, decreasing cover size

---
+++ components/modals/ItemDetailsModal.vue
@@ -0,0 +1,52 @@
+<template>
+  <modals-modal v-model="show" :width="400" height="100%">
+    <template #outer>
+      <div class="absolute top-5 left-4 z-40">
+        <p class="text-white text-2xl truncate">Details</p>
+      </div>
+    </template>
+
+    <div class="w-full h-full overflow-hidden absolute top-0 left-0 flex items-center justify-center" @click="show = false">
+      <div class="w-full overflow-x-hidden overflow-y-auto bg-primary rounded-lg border border-white border-opacity-20 p-2" style="max-height: 75%" @click.stop>
+        <p class="mb-1">{{ mediaMetadata.title }}</p>
+        <p class="mb-1 text-xs text-gray-200">ID: {{ _libraryItem.id }}</p>
+      </div>
+    </div>
+  </modals-modal>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Boolean,
+    libraryItem: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    show: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        this.$emit('input', val)
+      }
+    },
+    _libraryItem() {
+      return this.libraryItem || {}
+    },
+    media() {
+      return this._libraryItem.media || {}
+    },
+    mediaMetadata() {
+      return this.media.metadata || {}
+    }
+  },
+  methods: {},
+  mounted() {}
+}
+</script>
