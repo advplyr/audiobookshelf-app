b8b2d73eab6b0fc0b766d8edcd60f4f0d5de71f2
advplyr <advplyr@protonmail.com>
2022-08-21 15:52:06 -0500

    Sleep timer UI update and min custom time to 1 min

--- components/modals/SleepTimerModal.vue
+++ components/modals/SleepTimerModal.vue
@@ -6,13 +6,17 @@
       </div>
     </template>
 
-    <div class="w-full h-full overflow-hidden absolute top-0 left-0 flex items-center justify-center" @click="show = false; manualTimerModal = false">
+    <div
+      class="w-full h-full overflow-hidden absolute top-0 left-0 flex items-center justify-center"
+      @click="
+        show = false
+        manualTimerModal = false
+      "
+    >
       <div class="w-full overflow-x-hidden overflow-y-auto bg-primary rounded-lg border border-white border-opacity-20" style="max-height: 75%" @click.stop>
-        <div v-show="manualTimerModal" class="px-2 py-4">
-          <div class="text-gray-50 select-none relative py-3 pl-9" @click="manualTimerModal = false">
-            <div class="absolute right-1 top-0 bottom-0 h-full flex items-center">
-              <span class="material-icons text-2xl">close</span>
-            </div>
+        <div v-if="manualTimerModal" class="p-4">
+          <div class="flex mb-4" @click="manualTimerModal = false">
+            <span class="material-icons text-3xl">arrow_back</span>
           </div>
           <div class="flex my-2 justify-between">
             <ui-btn @click="decreaseManualTimeout" class="w-9 h-9" :padding-x="0" small style="max-width: 36px"><span class="material-icons">remove</span></ui-btn>
@@ -21,7 +25,7 @@
           </div>
           <ui-btn @click="clickedOption(manualTimeoutMin)" class="w-full">Set Timer</ui-btn>
         </div>
-        <ul v-show="!manualTimerModal" v-if="!sleepTimerRunning" class="h-full w-full" role="listbox" aria-labelledby="listbox-label">
+        <ul v-else-if="!sleepTimerRunning" class="h-full w-full" role="listbox" aria-labelledby="listbox-label">
           <template v-for="timeout in timeouts">
             <li :key="timeout" class="text-gray-50 select-none relative py-4 cursor-pointer hover:bg-black-400" role="option" @click="clickedOption(timeout)">
               <div class="flex items-center justify-center">
@@ -40,7 +44,7 @@
             </div>
           </li>
         </ul>
-        <div v-else class="px-2 py-4">
+        <div v-else class="p-4">
           <div class="flex my-2 justify-between">
             <ui-btn @click="decreaseSleepTime" class="w-9 h-9" :padding-x="0" small style="max-width: 36px"><span class="material-icons">remove</span></ui-btn>
             <p class="text-2xl font-mono text-center">{{ timeRemainingPretty }}</p>
@@ -65,7 +69,7 @@ export default {
   data() {
     return {
       manualTimerModal: null,
-      manualTimeoutMin: 0,
+      manualTimeoutMin: 1
     }
   },
   computed: {
@@ -109,7 +113,7 @@ export default {
       this.manualTimeoutMin++
     },
     decreaseManualTimeout() {
-      if (this.manualTimeoutMin > 0) this.manualTimeoutMin--
+      if (this.manualTimeoutMin > 1) this.manualTimeoutMin--
     }
   },
   mounted() {}
