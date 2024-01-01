69fbfa4b331cf13da819082b1e2adec0a20648c4
advplyr <advplyr@protonmail.com>
2024-01-01 10:19:55 -0600

    Update nuxtjs/tailwindcss and update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: {
-    safelist: [
-      'bg-success',
-      'bg-info',
-      'text-info'
-    ]
-  },
+  content: [
+    'components/**/*.vue',
+    'layouts/**/*.vue',
+    'pages/**/*.vue',
+    'mixins/**/*.js',
+    'plugins/**/*.js'
+  ],
   theme: {
     extend: {
       screens: {
