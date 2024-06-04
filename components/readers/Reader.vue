a8172fb3f9d49b03c58703651166234e79613703
Faizan Zafar <faizan.zafar01@student.csulb.edu>
2024-06-03 20:25:11 -0700

    Update font settings in EpubReader and Reader components to include a "Font Boldness" slider which applies a stroke to the text #3020 on main repo

--- components/readers/Reader.vue
+++ components/readers/Reader.vue
@@ -82,6 +82,12 @@
               </div>
               <ui-range-input v-model="ereaderSettings.lineSpacing" :min="100" :max="300" :step="5" input-width="180px" @input="settingsUpdated" />
             </div>
+            <div class="flex items-center mb-6">
+              <div class="w-32">
+                <p class="text-base">{{ $strings.LabelFontBoldness }}:</p>
+              </div>
+              <ui-range-input v-model="ereaderSettings.textStroke" :min="0" :max="300" :step="5" input-width="180px" @input="settingsUpdated" />
+            </div>
             <div class="flex items-center">
               <div class="w-32">
                 <p class="text-base">{{ $strings.LabelLayout }}:</p>
@@ -116,7 +122,8 @@ export default {
         theme: 'dark',
         fontScale: 100,
         lineSpacing: 115,
-        spread: 'auto'
+        spread: 'auto',
+        textStroke: 0
       }
     }
   },
