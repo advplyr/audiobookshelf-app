eb2aa6f7732d9c68783952ec72f912a0fa7dd894
advplyr <advplyr@protonmail.com>
2024-06-10 16:38:29 -0500

    Map locally stored ebook settings onto defaults

--- components/readers/Reader.vue
+++ components/readers/Reader.vue
@@ -376,12 +376,12 @@ export default {
       try {
         const settings = localStorage.getItem('ereaderSettings')
         if (settings) {
-          const ereaderSettings = JSON.parse(settings)
-          if (!ereaderSettings.spread) {
-            // Added in 0.9.71
-            ereaderSettings.spread = 'auto'
+          const _ereaderSettings = JSON.parse(settings)
+          for (const key in this.ereaderSettings) {
+            if (_ereaderSettings[key] !== undefined) {
+              this.ereaderSettings[key] = _ereaderSettings[key]
+            }
           }
-          this.ereaderSettings = ereaderSettings
           this.settingsUpdated()
         }
       } catch (error) {
