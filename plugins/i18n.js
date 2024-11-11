78f0c8823d76f5efc2db5a45e208cd2be04e2b54
advplyr <advplyr@protonmail.com>
2024-11-11 16:41:21 -0600

    Add Bengali and Slovenian to language setting dropdown

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -5,6 +5,7 @@ const defaultCode = 'en-us'
 let $localStore = null
 
 const languageCodeMap = {
+  bn: { label: 'বাংলা', dateFnsLocale: 'bn' },
   cs: { label: 'Čeština', dateFnsLocale: 'cs' },
   da: { label: 'Dansk', dateFnsLocale: 'da' },
   de: { label: 'Deutsch', dateFnsLocale: 'de' },
@@ -21,6 +22,7 @@ const languageCodeMap = {
   pl: { label: 'Polski', dateFnsLocale: 'pl' },
   'pt-br': { label: 'Português (Brasil)', dateFnsLocale: 'ptBR' },
   ru: { label: 'Русский', dateFnsLocale: 'ru' },
+  sl: { label: 'Slovenščina', dateFnsLocale: 'sl' },
   sv: { label: 'Svenska', dateFnsLocale: 'sv' },
   uk: { label: 'Українська', dateFnsLocale: 'uk' },
   'vi-vn': { label: 'Tiếng Việt', dateFnsLocale: 'vi' },
