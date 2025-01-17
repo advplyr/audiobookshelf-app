8268592e8e8838fa4eedee9ae132af896ab7be63
advplyr <advplyr@protonmail.com>
2025-01-17 17:06:50 -0600

    Update cordova-plugin-screen-orientation, update postcss in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,59 +19,36 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'viewport-fit=cover, width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
+    meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'viewport-fit=cover, width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1' }, { hid: 'description', name: 'description', content: '' }, { name: 'format-detection', content: 'telephone=no' }],
     script: [
       {
         src: '/libs/sortable.js'
       }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
-  css: [
-    '@/assets/tailwind.css',
-    '@/assets/app.css'
-  ],
+  css: ['@/assets/tailwind.css', '@/assets/app.css'],
 
-  plugins: [
-    '@/plugins/server.js',
-    '@/plugins/db.js',
-    '@/plugins/localStore.js',
-    '@/plugins/init.client.js',
-    '@/plugins/axios.js',
-    '@/plugins/nativeHttp.js',
-    '@/plugins/capacitor/index.js',
-    '@/plugins/capacitor/AbsAudioPlayer.js',
-    '@/plugins/toast.js',
-    '@/plugins/constants.js',
-    '@/plugins/haptics.js',
-    '@/plugins/i18n.js'
-  ],
+  plugins: ['@/plugins/server.js', '@/plugins/db.js', '@/plugins/localStore.js', '@/plugins/init.client.js', '@/plugins/axios.js', '@/plugins/nativeHttp.js', '@/plugins/capacitor/index.js', '@/plugins/capacitor/AbsAudioPlayer.js', '@/plugins/toast.js', '@/plugins/constants.js', '@/plugins/haptics.js', '@/plugins/i18n.js'],
 
   components: true,
 
-  modules: [
-    '@nuxtjs/axios'
-  ],
+  modules: ['@nuxtjs/axios'],
 
   axios: {},
 
   build: {
     postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {}
+        }
+      }
     },
     babel: {
-      plugins: [['@babel/plugin-proposal-private-property-in-object', { loose: true }]],
-    },
+      plugins: [['@babel/plugin-proposal-private-property-in-object', { loose: true }]]
+    }
   }
 }
