af5eec4d657853e162f8b5367571abc08014ad29
Andy Kelk <andy@andykelk.net>
2025-07-25 09:49:49 +1000

    feat: allow queue access from the side drawer
    If nothing is playing, we need a way to access the queue. Add this as an
    item in the side drawer.
    
    This uses the same modal as is available from the player view.

--- components/app/SideDrawer.vue
+++ components/app/SideDrawer.vue
@@ -90,6 +90,11 @@ export default {
           icon: 'home',
           text: this.$strings.ButtonHome,
           to: '/bookshelf'
+        },
+        {
+          icon: 'queue_music',
+          text: this.$strings.LabelQueue,
+          action: 'openQueue'
         }
       ]
       if (!this.serverConnectionConfig) {
@@ -173,6 +178,9 @@ export default {
         this.show = false
         let path = `/library/${this.$store.state.libraries.currentLibraryId}`
         await this.$store.dispatch('user/openWebClient', path)
+      } else if (action === 'openQueue') {
+        this.show = false
+        this.$store.commit('globals/setShowQueueModal', true)
       }
     },
     clickBackground() {
