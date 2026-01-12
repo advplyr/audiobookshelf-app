fada8fccb1ebd436c003442ff719ea5d095e3949
KiwiHour <matthew.teabear.basford@gmail.com>
2026-01-12 00:07:34 +0000

    made order-by menu scrollable

--- components/modals/OrderModal.vue
+++ components/modals/OrderModal.vue
@@ -1,7 +1,7 @@
 <template>
   <modals-modal v-model="show" width="90%">
     <div class="w-full h-full bg-primary rounded-lg border border-fg/20">
-      <ul class="w-full rounded-lg text-base" role="listbox" aria-labelledby="listbox-label">
+      <ul class="w-full rounded-lg text-base max-h-[70vh] overflow-y-auto overscroll-contain" role="listbox" aria-labelledby="listbox-label">
         <template v-for="item in items">
           <li :key="item.value" class="text-fg select-none relative py-4 pr-9 cursor-pointer" :class="item.value === selected ? 'bg-bg bg-opacity-50' : ''" role="option" @click="clickedOption(item.value)">
             <div class="flex items-center">
