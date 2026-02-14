f694a8340a77fff3dba522bdd05a1390b4486117
Adam Guymon <adambguymon@gmail.com>
2026-02-14 02:50:24 +0000

    Add multi-select mode for batch bookmark deletion
    Long-press a bookmark to enter multi-select mode, then tap to
    select/deselect additional bookmarks. Includes select all toggle
    and batch delete with confirmation dialog. The create bookmark
    button is hidden during selection mode. Adds i18n strings for
    the new UI elements.

--- components/modals/bookmarks/BookmarkItem.vue
+++ components/modals/bookmarks/BookmarkItem.vue
@@ -1,5 +1,8 @@
 <template>
-  <div :key="bookmark.time" :id="`bookmark-row-${bookmark.time}`" class="flex items-center px-1 py-4 justify-start relative" :class="highlight ? 'bg-bg bg-opacity-60' : ' bg-opacity-20'" @click="click">
+  <div :key="bookmark.time" :id="`bookmark-row-${bookmark.time}`" class="flex items-center px-1 py-4 justify-start relative" :class="highlight ? 'bg-bg bg-opacity-60' : ' bg-opacity-20'" @click="click" @longpress="onLongPress" @touchstart="touchStart" @touchend="touchEnd" @touchmove="touchMove">
+    <div v-if="selectMode" class="flex items-center justify-center w-10 h-10 cursor-pointer" @click.stop="toggleSelect">
+      <span class="material-symbols text-2xl" :class="selected ? 'text-success fill' : 'text-fg-muted'">{{ selected ? 'check_circle' : 'radio_button_unchecked' }}</span>
+    </div>
     <div class="flex-grow overflow-hidden px-2">
       <div class="flex items-center mb-0.5">
         <i class="material-symbols text-lg pr-1 -mb-1" :class="{ 'text-success fill': highlight, 'text-fg-muted': !highlight }">bookmark</i>
@@ -9,7 +12,7 @@
       </div>
       <p class="text-sm font-mono text-fg-muted flex items-center"><span class="material-symbols text-base pl-px pr-1">schedule</span>{{ $secondsToTimestamp(bookmark.time / playbackRate) }}</p>
     </div>
-    <div class="h-full flex items-center justify-end transform w-16 pr-2" @click.stop>
+    <div v-if="!selectMode" class="h-full flex items-center justify-end transform w-16 pr-2" @click.stop>
       <span class="material-symbols text-2xl mr-2 text-fg hover:text-yellow-400" @click.stop="editClick">edit</span>
       <span class="material-symbols text-2xl text-fg hover:text-error" @click.stop="deleteClick">delete</span>
     </div>
@@ -24,14 +27,22 @@ export default {
       default: () => {}
     },
     highlight: Boolean,
-    playbackRate: Number
+    playbackRate: Number,
+    selectMode: Boolean,
+    selected: Boolean
   },
   data() {
-    return {}
+    return {
+      longPressTimer: null,
+      longPressTriggered: false
+    }
   },
-  computed: {},
   methods: {
     click() {
+      if (this.selectMode) {
+        this.toggleSelect()
+        return
+      }
       this.$emit('click', this.bookmark)
     },
     deleteClick() {
@@ -39,7 +50,32 @@ export default {
     },
     editClick() {
       this.$emit('edit', this.bookmark)
+    },
+    toggleSelect() {
+      this.$emit('toggle-select', this.bookmark)
+    },
+    onLongPress() {
+      this.$emit('long-press', this.bookmark)
+    },
+    touchStart(e) {
+      this.longPressTriggered = false
+      this.longPressTimer = setTimeout(() => {
+        this.longPressTriggered = true
+        this.onLongPress()
+      }, 500)
+    },
+    touchEnd(e) {
+      if (this.longPressTriggered) {
+        e.preventDefault()
+      }
+      clearTimeout(this.longPressTimer)
+    },
+    touchMove() {
+      clearTimeout(this.longPressTimer)
     }
+  },
+  beforeDestroy() {
+    clearTimeout(this.longPressTimer)
   }
 }
 </script>
