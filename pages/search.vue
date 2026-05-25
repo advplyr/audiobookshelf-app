bd0cbf8bf0db4187dafe7128cff33ae82575d11f
OguzHAN <oguzzh4nn@gmail.com>
2026-05-25 21:12:38 +0300

    Remove hardcoded search result limit of 5
    The search API call had a hardcoded limit=5 parameter which restricted
    search results to only 5 items regardless of how many matches exist.
    Removing this limit allows the server's default pagination to apply,
    showing all relevant search results to the user.
    
    Fixes #1872

--- pages/search.vue
+++ pages/search.vue
@@ -122,7 +122,7 @@ export default {
         return
       }
       this.isFetching = true
-      const results = await this.$nativeHttp.get(`/api/libraries/${this.currentLibraryId}/search?q=${value}&limit=5`).catch((error) => {
+      const results = await this.$nativeHttp.get(`/api/libraries/${this.currentLibraryId}/search?q=${value}`).catch((error) => {
         console.error('Search error', error)
         return null
       })
